8468ca00eb6f3feab816c0000411e4feac3929aa
ValdasMiklovas <97112731+ValdasMiklovas@users.noreply.github.com>
2022-03-15 20:28:33 +0200

    Lessons Works

---
+++ AntraPamoka/AntraPamoka/Program.cs
@@ -0,0 +1,244 @@
+﻿using System;
+
+namespace AntraPamoka
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Antra pamoka TEMA: Kintamieji
+            //Užduotis Nr.1
+
+            //int yearOfBirth; //Inicializacija
+            //string firstName;
+
+            //var lastName = "Miklovas";//deklaruojam
+            //decimal salary = 10m; //darbui su pinigais
+            //double income = 5000d; //darbui su skaičiams su liekana
+
+            //yearOfBirth = 1989; //Reikšmės priskyrimas
+            //firstName = "Valdas";
+            //DateTime lastPayDay = new DateTime(2022, 03, 07);
+            //DateTime date = DateTime.Today.AddDays(2);
+
+            //Console.WriteLine($"First name:{firstName}, Last name:{lastName}, Year of birth:{yearOfBirth}");
+
+            //Užduotis nr1
+
+            // Užduotis Nr.2
+            //string schoolName;
+            //string courseName;
+            //int studentNumber;
+
+            //schoolName = "Code Acadamy";
+            //courseName = ".Net mokymai";
+            //DateTime date = DateTime.Today;
+
+            //Console.WriteLine($"School - {schoolName} ");
+            //Console.WriteLine($"Course -  {courseName}");
+            //Console.WriteLine($"Date -  {date}");
+
+            // Užduotis Nr.3
+            //string schoolname;
+            //string coursename;
+            //int studentnumber;
+
+            //schoolname = "code acadamy";
+            //coursename = ".net mokymai";
+            //datetime date = datetime.today;
+            //datetime startdate = new datetime(2022, 03, 01);
+            //datetime enddate = new datetime(2022, 10, 21);
+            //double  totaldays = (enddate - startdate).totaldays;
+            //double weeks = totaldays / 7;
+            //double workdays = totaldays - (weeks * 3);
+            ////string dateformat = date.tolongdatestring("mm/dd/yyyy"); //-datos formatavimui
+
+
+            //console.writeline($"school - {schoolname} ");
+            //console.writeline($"course -  {coursename}");
+            //console.writeline($"today day - {date}");
+            //console.writeline($"start date -  {startdate}");
+            //console.writeline($"end date - {enddate}");
+            //console.writeline($"course days - {totaldays}");
+            //console.writeline($"weeks numbers - {weeks}");
+            //console.writeline($"days without friday and weekends - {workdays}");
+
+            //Užduotis Nr4
+
+            //Console.WriteLine("Enter first Number");
+            //string input1 = Console.ReadLine();
+
+            //bool isNumber  = Int32.TryParse(input1, out int number1);
+            //if(isNumber)
+            //{
+            //    Console.WriteLine("Enter first Number");
+            //    string input2 = Console.ReadLine();
+            //    bool isNumber2 = Int32.TryParse(input2,out int number2);
+
+            //    if (isNumber2)
+            //    {
+            //        bool equal = number1 == number2;
+
+            //        Console.WriteLine($" Is Numbers {number1} and {number2} is equal? {equal}");
+            //    }
+
+            //    else
+            //    {
+            //        Console.WriteLine("bad input2");
+            //    }
+
+
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Bad input1");
+            //}
+
+            //Užduotis Nr5
+            //su begaliniu ciklu
+
+            //bool valid = true;
+
+            //do
+            //{
+            //    Console.WriteLine("Enter the first number");
+            //    string iput1 = Console.ReadLine();
+            //    bool isThisNumber1 = Int32.TryParse(iput1, out int number1);
+            //    if (isThisNumber1)
+            //    {
+            //        Console.WriteLine("Enter the second number");
+            //        string input2 = Console.ReadLine();
+            //        bool isThisNumber2 = Int32.TryParse(input2, out int number2);
+
+            //        if (isThisNumber2)
+            //        {
+            //            number1++;
+            //            number2--;
+
+            //            Console.WriteLine($"First number: {number1}");
+            //            Console.WriteLine($"Second number: {number2}");
+            //            valid = false;
+            //        }
+            //        else
+            //        {
+            //            Console.WriteLine("Wrong input");
+            //        }
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Wrong input");
+
+            //    }
+
+            //}
+            //while (valid);
+
+
+            //užduotis be begalinio ciklo.
+
+            //Console.WriteLine("Enter the first number");
+            //string iput1 = Console.ReadLine();
+            //bool isThisNumber1 = Int32.TryParse(iput1, out int number1);
+            //if (isThisNumber1)
+            //{
+            //    Console.WriteLine("Enter the second number");
+            //    string input2 = Console.ReadLine();
+            //    bool isThisNumber2 = Int32.TryParse(input2, out int number2);
+
+            //    if (isThisNumber2)
+            //    {
+            //        number1++;
+            //        number2--;
+
+            //        Console.WriteLine($"First number: {number1}");
+            //        Console.WriteLine($"Second number: {number2}");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Wrong input");
+            //    }
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Wrong input");
+            //}
+
+            //Užnduotis Nr.6
+
+
+
+            //Console.WriteLine("enter text");
+            //string text = Console.ReadLine();
+            //bool isempty = text == string.Empty;
+            //if (isempty)
+            //{
+            //    Console.WriteLine($"Line is Empty->{isempty}");
+
+            //}
+            //else
+            //{
+            //    Console.WriteLine($"Line is Empty -> {isempty}");
+            //}
+
+            // Užduotis Nr7
+
+            //bool valid = true;
+            //do
+            //{
+            //    Console.WriteLine("Enter the number");
+            //    string input = Console.ReadLine();
+
+
+            //    bool number = Double.TryParse(input, out double number1);
+
+
+            //    if (number)
+            //    {
+            //        bool result = number1 < 0;
+            //        Console.WriteLine($"Does {number1}<0, {result}");
+            //        valid = false;
+
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Wrong input");
+            //    }
+            //}
+            //while (valid);
+
+            //Užduotis Nr8.
+
+            bool valid = true;
+            string userFirstName = "Valdas";
+            string userLastName = "Miklovas";
+            //StringCimparison.OrdinalIgnoreCase
+
+            
+            do
+            {
+                Console.WriteLine("Enter the User first Name:");
+                string inputUserFirstName = Console.ReadLine();
+                Console.WriteLine("Enter the Usere Last Name");
+                string inputUserLastName = Console.ReadLine();
+                if (string.Equals(userFirstName,inputUserFirstName,StringComparison.OrdinalIgnoreCase)||string.Equals(userLastName,inputUserLastName,StringComparison.OrdinalIgnoreCase))
+                {
+                    Console.WriteLine("Match, you connected");
+                    valid = false;
+                }
+
+                else
+                {
+                    Console.WriteLine("User First Name or Last Name incorrect. Try again");
+                }
+
+            }
+            while (valid);
+
+
+
+
+
+
+        }
+    }
+}
---
+++ PirmaPaskaita/PirmaPaskaita/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+
+namespace PirmaPaskaita
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ////komentaras
+            //string name = "";
+
+            //while(name != "stop")
+            //{
+            //    Console.WriteLine("enter the name");
+            //    name = Console.ReadLine();
+            //    Console.WriteLine($"Sveiki{name}");
+            //}
+
+            //Antra paskaita 2022-03-07
+            //1nr užduotis parašyti programą kuri klausia varototoji megstamos spalvos, programa turi visada atsakyti Mano irgi!
+            //string Color = "";
+            //while (Color != "stop")
+            //{
+            //    Console.WriteLine("What is your favour color?");
+            //    Color = Console.ReadLine();
+
+            //    Console.WriteLine($"{Color} also my favour color ");
+            //}
+            //Console.Clear();
+
+            //Užduotis Nr2
+            //int number1 = 17;
+            //int number2 = 23;
+
+            //Console.WriteLine($" suma {number2} + {number1}= {number1+number2}" );
+
+            //Užduotis Nr3
+
+            //double number1 = 24;
+            //double number2 = 5;
+            //Console.WriteLine($"{number1}/{number2}={number1 / number2}");
+
+            //užduotis Nr4
+            //Console.WriteLine(-6 + 3 * 5);
+            //Console.WriteLine((13 - 2) * 7);
+            //Console.WriteLine((5 + -2) + (20 / 10));
+
+            //užduotis Nr5
+            Console.WriteLine("Enter the first number");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the second number");
+            string number2 = Console.ReadLine();// Prieėma betkur simbolį
+            int number3;//Tryparse priskiriamas skaičius
+            Int32.Parse(number2, out number3);//tekstą keičia į skaičių, jeigu įvesta raidė priskiriama nuliui reikšmė.
+            //int number2 = Int32.Parse(Console.ReadLine());
+            //int number2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"{number1}*{number3}={number1 * number3}");
+           //Console.WriteLine($"{number1}/{number3}={number1 / number3}");// nesidalina iš nulio
+            Console.WriteLine($"{number1}+{number3}={number1 + number3}");
+            Console.WriteLine($"{number1}-{number3}={number1 - number3}");
+
+            ////Užduotis Nr6
+            ////Console.WriteLine("Enter the number");
+            ////int num = Convert.ToInt32(Console.ReadLine());
+            ////Console.WriteLine($"{num} x 1 = {num * 1}");
+            ////Console.WriteLine($"{num} x 2 = {num *2}");
+            ////Console.WriteLine($"{num} x 3 = {num * 3}");
+            ////Console.WriteLine($"{num} x 4 = {num * 4}");
+            ////Console.WriteLine($"{num} x 5 = {num * 5}");
+            ////Console.WriteLine($"{num} x 6 = {num * 6}");
+            ////Console.WriteLine($"{num} x 7 = {num * 7}");
+            ////Console.WriteLine($"{num} x 8 = {num * 8}");
+            ////Console.WriteLine($"{num} x 9 = {num * 9}");
+            ////Console.WriteLine($"{num} x 10 = {num * 10}");
+
+            // užduotis Nr7
+
+            //Console.WriteLine("Enter 4 numbers");
+            //int num1 = Convert.ToInt32(Console.ReadLine());
+            //int num2 = Convert.ToInt32(Console.ReadLine());
+            //int num3 = Convert.ToInt32(Console.ReadLine());
+            //int num4 = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine($"average = {(num1+num2+num3+num4)/4}");
+
+            //užduotis nr8
+            //Console.WriteLine("Enter 3 numbers");
+            //int numA = Convert.ToInt32(Console.ReadLine());
+            //int numB = Convert.ToInt32(Console.ReadLine());
+            //int numC = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine($"({numA}+{numB})*{numC}={(numA + numB) * numC}");
+            //Console.WriteLine($"{numA}*{numC}+{numB}*{numC}={numA * numC + numB * numC}");
+
+            //užduotis nr9
+            //Console.WriteLine("enter the simbol");
+            //string symbol = Console.ReadLine();
+
+            //Console.WriteLine($"{symbol}{symbol}{symbol}");
+            //Console.WriteLine($"{symbol} {symbol}");
+            //Console.WriteLine($"{symbol} {symbol}");
+            //Console.WriteLine($"{symbol} {symbol}");
+            //Console.WriteLine($"{symbol}{symbol}{symbol}");
+
+            //Užduotis Nr10
+            //Console.WriteLine("Enter the Temperature C");
+            //int tempC = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine($" Kelvis = {tempC}+ 273 = {tempC + 273} K");
+            //Console.WriteLine($"FARENHEIT = {tempC}* 18/10 + 32 = {tempC * (18 / 10) + 32} F");
+
+
+
+
+
+
+
+        }
+    }
+}
